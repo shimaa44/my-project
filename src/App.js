afb1827ce9861dc237dc65cdff178af888e7a857
shimaa44 <shimaaabouelezz8@gmail.com>
2021-01-23 14:38:40 +0200

    update

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Home';
+import Header from './Header';
+import Footer from './Footer'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
